afd16ce371492150fbcd114061182dc6658da968
Jennifer <jsmit032@ucr.edu>
2020-04-03 15:08:54 -0500

    Create new mongodb, successfully added three new items to an items Schema

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,12 +11,36 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const buyFood = new Item ({
+  name: "Buy Food"
+});
+
+const cookFood = new Item ({
+  name: "Cook Food"
+});
+
+const eatFood = new Item ({
+  name: "Eat Food"
+});
+
+Item.insertMany([buyFood, cookFood, eatFood], function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Succesfully logged all the list items!");
+  }
+});
 
 app.get("/", function(req, res){
-  const day = date.getDate();
-  res.render('list', {listTitle: day, newListItems: items});
+  res.render('list', {listTitle: "Today", newListItems: items});
 });
 
 app.post("/", function(req, res){
@@ -37,8 +61,4 @@ app.get('/work', function(req, res){
 
 app.get('/about', function(req, res){
   res.render("about");
-});
-
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
 });
